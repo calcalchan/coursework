b9a596625992a8f567554f917c76e525adf4187c
Your Name <your@email>
2018-12-06 18:59:34 +0000

    Added comments on controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
+  #Actions that are done before other actions
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  #Authenticiate whether the user is valid or not
   before_action :authenticate_user!, except: [:index, :show, :request_contact]
 
   #For the index page, if the page for the specific category is
@@ -32,7 +34,7 @@ class MoviesController < ApplicationController
   end
 
 
-  
+
   def new
     @movie = current_user.movies.build
     @categories = Category.order('name ASC').all.map{ |c| [c.name, c.id]}
@@ -51,9 +53,11 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
 
+      #If the movie saves, it shows the notice and redirect to the movie page.
       if @movie.save
         format.html { redirect_to @movie, notice: t('movie.create') }
         format.json { render :show, status: :created, location: @movie }
+      #If the movie doesn't saves, it refreshes or render the create movie page.
       else
         format.html { render :new }
         format.json { render json: @movie.errors, status: :unprocessable_entity }
@@ -62,13 +66,15 @@ class MoviesController < ApplicationController
     end
   end
 
-
+  #A method to update a movie
   def update
     @movie.category_id = params[:category_id]
     respond_to do |format|
+      #If the movie params are all filled in and valid, it redirects to the movie page and shows the notice
       if @movie.update(movie_params)
         format.html { redirect_to @movie, notice: t('movie.update') }
         format.json { render :show, status: :ok, location: @movie }
+      #If it doesnt update, then refresher the edit movie page
       else
         format.html { render :edit }
         format.json { render json: @movie.errors, status: :unprocessable_entity }
@@ -76,11 +82,12 @@ class MoviesController < ApplicationController
     end
   end
 
-
+  #Deleting a movie
   def destroy
     @movie.destroy
 
     respond_to do |format|
+      #It redirects to the movie home page and shows the notice.
       format.html { redirect_to movies_url, notice: t('movie.destroy') }
       format.json { head :no_content }
     end
@@ -91,8 +98,10 @@ class MoviesController < ApplicationController
   #Or else just display all the movies.
   def search
 
+      #If the the params searched in the bar is present then show that movie
       if params[:search].present?
         @movies = Movie.search(params[:search])
+      #If not just display all the movies
       else
         @movies = Movie.all
       end
@@ -123,7 +132,7 @@ class MoviesController < ApplicationController
       @movie = Movie.find(params[:id])
     end
 
-
+    #Using strong params
     def movie_params
       params.require(:movie).permit(:title, :category_id, :description, :movie_length, :director, :rating, :image)
     end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
   def edit
   end
 
-  #A method to create a review. It also associates the current review id to the
+  #A method to create a review and taking the review params. It also associates the current review id to the
   #current user and movie.
   def create
     @review = Review.new(review_params)
@@ -20,9 +20,11 @@ class ReviewsController < ApplicationController
     @review.movie_id = @movie.id
 
     respond_to do |format|
+      #If the review is saved, it redirects to the movie page and shows the notice.
       if @review.save
         format.html { redirect_to @movie, notice: t('review.create') }
         format.json { render :show, status: :created, location: @review }
+      #If the review is not saved, it refreshes the create review page.
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -30,12 +32,14 @@ class ReviewsController < ApplicationController
     end
   end
 
-
+  #A method to update the review
   def update
     respond_to do |format|
+      #If the params are filled in and valid, it redirects to the review page and shows the notice
       if @review.update(review_params)
         format.html { redirect_to @review, notice: t('review.update') }
         format.json { render :show, status: :ok, location: @review }
+      #If it doesnt update then it refreshes the edit review page
       else
         format.html { render :edit }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -43,10 +47,11 @@ class ReviewsController < ApplicationController
     end
   end
 
-
+  #A method to destroy a review
   def destroy
     @review.destroy
     respond_to do |format|
+      #If destroyed, it display the notice.
       format.html { redirect_to reviews_url, notice: t('review.destroy') }
       format.json { head :no_content }
     end
@@ -62,7 +67,7 @@ class ReviewsController < ApplicationController
       @movie = Movie.find(params[:movie_id])
     end
 
-
+    #Using strong params for the review
     def review_params
       params.require(:review).permit(:rating, :comment)
     end
